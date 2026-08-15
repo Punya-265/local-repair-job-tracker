08dd74607a04d056eb87fd53e5699021f7a4be25
Punya-265 <punyabindlish26@gmail.com>
2026-08-15 10:27:19 +0530

    Add customer signup route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const { body } = require('express-validator');
-const { login, getMe, updateProfile } = require('../controllers/authController');
+const { login, registerCustomer, getMe, updateProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 const validate = require('../middleware/validate');
 
 const router = express.Router();
 
-router.post(
-  '/login',
-  [
-    body('email').isEmail().withMessage('Please include a valid email'),
-    body('password').exists().withMessage('Password is required'),
-    validate,
-  ],
-  login
-);
+router.post('/register', [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Please include a valid email'),
+  body('phone').notEmpty().withMessage('Phone is required'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  validate,
+], registerCustomer);
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Please include a valid email'),
+  body('password').exists().withMessage('Password is required'),
+  validate,
+], login);
 
 router.get('/me', protect, getMe);
 router.put('/profile', protect, updateProfile);
